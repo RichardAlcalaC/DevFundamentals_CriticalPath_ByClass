dca7944925b9fc2fcdae873ba1124e3eda088bb7
RichardAlcalaC <jheranium@gmail.com>
2017-06-06 14:32:43 -0400

    final class 06-06-2017

---
+++ Circle.java
@@ -0,0 +1,17 @@
+
+
+public class Circle
+{
+    private int x;
+
+    public Circle()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    public int sampleMethod(int y)
+    {
+        return x + y;
+    }
+}
--- DrowingPanel.java
+++ DrowingPanel.java
@@ -1,18 +1,33 @@
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Graphics;
-
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class DrowingPanel extends JPanel
 {
     public DrowingPanel()
     {
-        setBackground(Color.blue);
+        setBackground(Color.GRAY);
+        addMouseListener(new MouseAdapter()
+        {
+            @Override
+            public void mouseClicked(MouseEvent event)
+            {
+                System.out.println(String.format("click at, %s, %s", event.getX(), event.getY()));
+            }
+        });
     }
     
     @Override
     public void paint(Graphics g)
     {
         super.paint(g);
+        g.setColor(Color.white);
+        int w=5;
+        int h=100;
+        int xCenter = getWidth()/2;
+        int yCenter = getHeight()/2;
+        g.fillOval(xCenter-(50/2), yCenter-(50/2), 50, 50);
     }
 }
